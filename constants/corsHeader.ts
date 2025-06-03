50ede098d520ff5e25452a9797395545977aac7d
aleksey <therayff@yandex.ru>
2025-06-04 00:54:38 +0700

    rewrite cors domain

--- constants/corsHeader.ts
+++ constants/corsHeader.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   headers: {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://greenworld-shop-admin.vercel.app',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   },
