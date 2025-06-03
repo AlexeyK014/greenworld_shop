10f8afe650f5aa00326ecb4356b1063632897485
aleksey <therayff@yandex.ru>
2025-06-04 01:51:34 +0700

    fixed news

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'lh3.googleusercontent.com' }],
-    remotePatterns: [{ protocol: 'http', hostname: 'localhost' }],
-    remotePatterns: [{ protocol: 'https', hostname: 'greenworld-shop.vercel.app' }],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'http', hostname: 'localhost' },
+      { protocol: 'https', hostname: 'greenworld-shop.vercel.app' }
+    ]
   },
 };
 
