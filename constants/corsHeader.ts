3877b85c7082b831cd98555baa44f82c3caa344f
aleksey <therayff@yandex.ru>
2025-06-05 09:01:03 +0700

    cors domain

--- constants/corsHeader.ts
+++ constants/corsHeader.ts
@@ -1,6 +1,7 @@
 export const corsHeaders = {
   headers: {
-    'Access-Control-Allow-Origin': 'https://greenworld-shop-admin.vercel.app',
+    // 'Access-Control-Allow-Origin': 'http://localhost:5173 ',
+    'Access-Control-Allow-Origin': 'https://greenworld-shop-admin.vercel.app ',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   },
