4b5c780bf459fda01a0659880f4e8faea8f21c09
aleksey <therayff@yandex.ru>
2025-06-06 00:46:47 +0700

    fixed new2

--- lib/utils/api-routes.ts
+++ lib/utils/api-routes.ts
@@ -18,29 +18,20 @@ export const getDbAndReqBody = async (clientPromise: Promise<MongoClient>, req: 
 };
 
 export const getNewAndBestsellerGoods = async (db: Db, fieldName: string) => {
-  const equipment = await db.collection('equipment').find().toArray();
-  const microgreen = await db.collection('microgreen').find().toArray();
+  const equipment = await db.collection('equipment').find({ [fieldName]: true }).toArray();
+  const microgreen = await db.collection('microgreen').find({ [fieldName]: true }).toArray();
 
-  // Фильтруем и добавляем поле category (если его ещё нет в БД)
-  const formattedEquipment = equipment
-    .filter(item => item[fieldName])
-    .slice(0, 2)
-    .map(item => ({ ...item, category: 'equipment' }));
-
-  const formattedMicrogreen = microgreen
-    .filter(item => item[fieldName])
-    .slice(0, 2)
-    .map(item => ({ ...item, category: 'microgreen' }));
-
-  // Возвращаем перемешанный массив
-  return shuffle([...formattedEquipment, ...formattedMicrogreen]);
+  return shuffle([
+    ...equipment.slice(0, 2).map(item => ({ ...item, category: 'equipment' })),
+    ...microgreen.slice(0, 2).map(item => ({ ...item, category: 'microgreen' })),
+  ]);
+  // const equipment = await db.collection('equipment').find().toArray();
+  // const microgreen = await db.collection('microgreen').find().toArray();
 
   // return shuffle([
   //   ...equipment
   //     .filter(
-  //       (item) =>
-  //         item[fieldName]
-  //     )
+  //       (item) => item[fieldName])
   //     // .filter((item) => item[fieldName] && Object.values(item).some((value) => value))
   //     .slice(0, 2),
   //   ...microgreen
